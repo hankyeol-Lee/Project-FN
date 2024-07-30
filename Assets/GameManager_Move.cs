009e691e6595e02c0457c0dfcd9b4a00a12bca86
hankyeol <lhn030703@gmail.com>
2024-07-31 06:16:06 +0900

    기능/RayCast 좌표받기
    -RayCast로 Tag가 "Cell"인 개체의
    Vector3Int값을 가져옴
    -디버깅 코드와 SetTile로 색을 바꾸는기능을 구현했음.(임시)

---
+++ Assets/GameManager_Move.cs
@@ -0,0 +1,55 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameManager_Move : MonoBehaviour
+{
+    public string cellTag = "Cell";
+    public Tilemap tilemap;
+    public TileBase tile;
+
+    private void Update()
+    {
+        GetRayCell();
+    }
+
+    public void GetRayCell() // 우클릭을 하면, 마우스 위치에 있는 타일을 가져오는 함수
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.Log("ray 쐈어");
+
+            RaycastHit2D[] hit = Physics2D.GetRayIntersectionAll(ray); //ray를 받은 '모든' 객체를 hit 변수에 저장.
+            Debug.Log("hit에 넣었어");
+            Vector3Int? returnCell = CheckCell(hit); //받아온 셀
+
+
+            //받아온 셀의 좌표 출력 및 settile로 이미지 변경.
+            if (returnCell.HasValue)
+            {
+                Vector3Int TargetCell = returnCell.Value;
+                Debug.Log($"타겟셀 : {TargetCell}");
+                tilemap.SetTile(TargetCell, tile);
+            }
+
+        }
+    }
+
+
+    Vector3Int? CheckCell(RaycastHit2D[] hit) //ray hit에서 cell만 걸러내는 함수. cell은 단 하나여야함.
+    {
+        foreach (var cell in hit)
+        {
+            if (cell.collider.CompareTag(cellTag)) // cellTag랑 똑같으면.
+            {
+                Vector3 worldPosition = cell.point;
+                Vector3Int cellPosition = tilemap.WorldToCell(worldPosition);
+                Debug.Log($"리턴 셀좌표 : {cellPosition}");
+                return cellPosition;
+            }
+        }
+        return null;
+    }
+
+}
