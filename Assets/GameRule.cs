55aa5bbc69a991a84e5ae8c17e58ce42f99af05b
seongmin_ <67692767+tjddlals@users.noreply.github.com>
2024-11-22 20:13:38 +0900

    GameRule 틀
    GameRule에서 전투 승/패 결정.
    이런 구조로 하면 안될 듯.

--- Assets/GameRule.cs
+++ Assets/GameRule.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 
 public class GameRule : MonoBehaviour
 {
-
+    public GameObject Player;
+    void Update()
+    {
+        if (Player.GetComponent<PlayerStatus>().playerHP <= 0)
+        {
+            //ÆÐ¹è
+        }
+        if (EnemyInstances.enemyDict.Count == 0)
+        {
+            //½Â¸®
+        }
+    }
 }
 
